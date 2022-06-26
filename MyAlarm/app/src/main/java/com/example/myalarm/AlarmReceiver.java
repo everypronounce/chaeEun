7d319a5a061ae72bd568d24f29f6450013a19b40
kche1106 <kche011106@gmail.com>
2022-06-26 18:03:08 +0900

    myalarm1

---
+++ MyAlarm/app/src/main/java/com/example/myalarm/AlarmReceiver.java
@@ -0,0 +1,25 @@
+package com.example.myalarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent sIntent = new Intent(context, AlarmService.class);
+
+        //Oreo(26)버전 이후부터는 BackGround에서 실행을 금지하기 때문에 ForeGround에서 실행해야함
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(sIntent);
+        }
+        else {
+            context.startService(sIntent);
+        }
+    }
+}
